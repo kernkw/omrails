d93887ade393d7f5b02b2398f13ac29bf94dec7d
Kyle Kern <kyle.sendgrid@gmail.com>
2013-03-11 13:25:24 -0600

    Intial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omrails::Application.config.secret_token = 'a0e3d90d07109048e809368ec904f3cd143fd76454a7905415763fa5dedda206246c2226c8ef9cd956ce5062df96722f138056e3c650fa81fbd6971f517efaac'
